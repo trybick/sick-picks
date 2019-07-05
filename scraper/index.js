d8015871c3f19b39556dbb36b76c3f48e38eaef2
trybick <t.rybicki@gmail.com>
2019-07-05 17:39:18 -0400

    Console log tagline as test

--- scraper/index.js
+++ scraper/index.js
@@ -1,14 +1,22 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
+const baseUrl = 'https://syntax.fm';
+
+async function scraper() {
+  // *** Setup ***
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto('https://syntax.fm');
+  await page.goto(baseUrl);
+  await page.waitFor(2000);
 
-  const tagline = await page.evaluate(() => {
-    const tag = document.querySelectorAll('h2.tagline').innerText;
-    return tag;
-  });
-
+  // *** Test ***
+  const tagline = await page.evaluate(() => document.querySelector('h2.tagline').textContent);
   console.log(tagline);
-})();
+
+  // *** Begin ***
+
+
+  browser.close();
+}
+
+scraper();
