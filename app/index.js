fda3ff474f443d0c769c148bddb4cc8170282317
trybick <t.rybicki@gmail.com>
2019-07-05 20:22:35 -0400

    Get lists of items and hrefs

--- app/index.js
+++ app/index.js
@@ -3,16 +3,27 @@ const puppeteer = require('puppeteer');
 const baseUrl = 'https://syntax.fm';
 
 async function scrapeSickPicks() {
-  // Setup
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   await page.goto(baseUrl, { waitUntil: 'networkidle0' });
 
   // Begin
-  const sick = await page.evaluate(() => document.querySelector('#-siiiiick-piiiicks-').textContent);
-  console.log(sick);
+  // const title = await page.evaluate(() => document.querySelectorAll('#-siiiiick-piiiicks- + ul li').textContent);
+  // console.log('title', title);
 
-  // Exit
+  const textList = await page.evaluate(() => {
+    const items = [...document.querySelectorAll('#-siiiiick-piiiicks- + ul li')];
+    return items.map(i => i.textContent);
+  });
+  console.log('text items:', textList);
+
+  const hrefList = await page.evaluate(() => {
+    const links = [...document.querySelectorAll('#-siiiiick-piiiicks- + ul li a')];
+    return links.map(a => a.href);
+  });
+  console.log('hrefs:', hrefList);
+
+
   browser.close();
 }
 
