57d59c8f4253e1f99face6a4238a570463a0f0fb
trybick <t.rybicki@gmail.com>
2019-07-06 14:14:34 -0400

    Click each show and scrape text and links

--- app/index.js
+++ app/index.js
@@ -3,48 +3,32 @@ const puppeteer = require('puppeteer');
 const baseUrl = 'https://syntax.fm';
 
 async function scrapeSickPicks() {
-  const browser = await puppeteer.launch({ headless: false, defaultViewport: null, devtools: true });
+  const browser = await puppeteer.launch({ headless: false, defaultViewport: null, devtools: false });
   const page = await browser.newPage();
   await page.goto(baseUrl, { waitUntil: 'networkidle0' });
 
-  // Get text and urls from first episode
-  // const textList = await page.evaluate(() => {
-  //   const items = [...document.querySelectorAll('#-siiiiick-piiiicks- + ul li')];
-  //   return items.map(i => i.textContent);
-  // });
-  // const hrefList = await page.evaluate(() => {
-  //   const links = [...document.querySelectorAll('#-siiiiick-piiiicks- + ul li a')];
-  //   return links.map(a => a.href);
-  // });
-  // console.log('text:', textList);
-  // console.log('links:', hrefList);
+  const numberOfShows = (await page.$$('.show')).length;
 
-  // // Navigate to next episode
-  // const shows = await page.evaluate(() => {
-  //   const list = [];
-  //   const elements = document.getElementsByClassName('show');
-  //   for (const el of elements) { list.push(el.textContent); }
-  //   return list;
-  // });
-  // console.log('shows', shows);
+  const scrapedData = [];
+  for (let n = 1; n < numberOfShows; n++) {
+    const textList = await page.evaluate(() => {
+      const items = [...document.querySelectorAll('#-siiiiick-piiiicks- + ul li')];
+      return items.map(i => i.textContent);
+    });
+    const hrefList = await page.evaluate(() => {
+      const links = [...document.querySelectorAll('#-siiiiick-piiiicks- + ul li a')];
+      return links.map(a => a.href);
+    });
+    scrapedData.push(textList, hrefList);
+    await Promise.all([
+      page.click(`#main > div.showList > div:nth-child(${n})`),
+      page.waitForNavigation({ waitUntil: 'networkidle2' }),
+    ]);
+  }
+  console.log('test', scrapedData);
 
-  const sickPicks = await page.evaluate(() => {
-    // const list = [];
-    // const shows = document.getElementsByClassName('show');
-    // for (const show of shows) { list.push(show.textContent); }
 
-    const shows = document.getElementsByClassName('show');
-    for (const show of shows) {
-      show.click();
-      console.log('a', [...document.querySelectorAll('#-siiiiick-piiiicks- + ul li')].map(i => i.textContent))
-    }
-
-    return shows;
-  });
-
-  console.log('sickPicks', sickPicks);
-
-  // browser.close();
+  browser.close();
 }
 
 scrapeSickPicks();
